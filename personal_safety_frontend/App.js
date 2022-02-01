b4650e9cfed4844d79b349d13cb73a8e787fec64
ayman fakri <61848289+afakri@users.noreply.github.com>
2022-01-31 21:30:04 -0500

    used redux to manage states

--- personal_safety_frontend/App.js
+++ personal_safety_frontend/App.js
@@ -5,44 +5,64 @@ import Friends from "./screens/Friends";
 import Profile from "./screens/Profile";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 const Stack = createNativeStackNavigator();
+const initialState = {
+  checked: false,
+};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SET_CHECKED":
+      return { checked: true };
+      break;
+    case "SET_NOT_CHECKED":
+      return { checked: false };
+      break;
+  }
+
+  return state;
+};
+const store = createStore(reducer);
 
 export default function App() {
   return (
     <>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-            animationEnabled: false,
-          }}
-          initialRouteName="Home"
-        >
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false,
               animationEnabled: false,
             }}
-          />
+            initialRouteName="Home"
+          >
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{
+                animationEnabled: false,
+              }}
+            />
 
-          <Stack.Screen
-            name="Friends"
-            component={Friends}
-            options={{
-              animationEnabled: true,
-            }}
-          />
-          <Stack.Screen
-            name="Profile"
-            component={Profile}
-            options={{
-              animationEnabled: false,
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+            <Stack.Screen
+              name="Friends"
+              component={Friends}
+              options={{
+                animationEnabled: true,
+              }}
+            />
+            <Stack.Screen
+              name="Profile"
+              component={Profile}
+              options={{
+                animationEnabled: false,
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
     </>
   );
 }
--- personal_safety_frontend/components/Main.js
+++ personal_safety_frontend/components/Main.js
@@ -1,39 +1,38 @@
 import { View, StyleSheet, Text } from "react-native";
 import Button from "./Button";
 import { useState, useEffect } from "react";
+import { connect } from "react-redux";
 
-function Main() {
-  const [checked, setChecked] = useState(false);
-
+function Main(props) {
   useEffect(() => {
-    if (checked) {
+    if (props.checked) {
       const timer = setTimeout(() => {
         console.log("10sec has passed! checkin now");
-        setChecked(false);
+        props.setNotChecked();
       }, 10000);
 
       return () => clearTimeout(timer);
     }
     const interval = setInterval(() => {
       console.log("Not Checked in ");
-      if (checked) {
+      if (props.checked) {
         return () => clearInterval(interval);
       }
     }, 10000);
     return () => clearInterval(interval);
-  }, [checked]);
+  }, [props.checked]);
 
   return (
     <View style={styles.page}>
       <View style={styles.container}>
         <Text style={styles.title}>Welcome, Sussan! </Text>
         <Text style={styles.description}>
-          {checked
+          {props.checked
             ? `You are checked-in.\nWe will keep you posted!`
             : `You have not yet checked-in today. \nTap the button to checkin now!`}
         </Text>
       </View>
-      {checked ? <></> : <Button onPress={setChecked} />}
+      {props.checked ? <></> : <Button onPress={props.setChecked} />}
     </View>
   );
 }
@@ -66,4 +65,17 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Main;
+function mapStateToProps(state) {
+  return {
+    checked: state.checked,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    setChecked: () => dispatch({ type: "SET_CHECKED" }),
+    setNotChecked: () => dispatch({ type: "SET_NOT_CHECKED" }),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
